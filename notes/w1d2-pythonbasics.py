8cbf64f8cc99437d8b1ecfd61b346cc58b8da74e
Roger Sole <rogersolesotillo@gmail.com>
2022-08-15 09:06:03 +0200

    Add typed function

--- notes/w1d2-pythonbasics.py
+++ notes/w1d2-pythonbasics.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 beatles = {
     'john': {
         'instruments': ['drums', 'bass'],
@@ -23,3 +25,18 @@ def full_name(first, last, capitalize=True):
 
 print(full_name('roger', 'sole'))
 print(full_name('roger', 'sole', False))
+
+
+def luhn_checksum(number: str) -> bool:
+
+    def digits_of(num: str) -> list[int]:
+        return [int(d) for d in num]
+
+    digits = digits_of(number)
+    odd_digits = digits[-1::2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for digit in even_digits:
+        checksum += sum(digits_of(str(digit * 2)))
+    return checksum % 10 == 0
